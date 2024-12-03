be751806aa367005cadfe8e1b76622e7f12cdb55
somehow <snsdkf6@gmail.com>
2024-12-03 16:42:09 +0900

    Icons and Transforms

--- lib/main.dart
+++ lib/main.dart
@@ -133,6 +133,8 @@ class App extends StatelessWidget {
                   height: 20,
                 ),
                 Container(
+                    // overflow일 때 튀어나온 부분을 어떻게 처리할지 정함
+                    clipBehavior: Clip.hardEdge,
                     decoration: BoxDecoration(
                       color: const Color(0xFF1F2123),
                       borderRadius: BorderRadius.circular(25),
@@ -140,6 +142,7 @@ class App extends StatelessWidget {
                     child: Padding(
                       padding: const EdgeInsets.all(20),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -174,6 +177,19 @@ class App extends StatelessWidget {
                                 ],
                               )
                             ],
+                          ),
+                          // overflowing 크기 변경
+                          Transform.scale(
+                            scale: 2.2,
+                            // 좌표 변경
+                            child: Transform.translate(
+                              offset: const Offset(-5, 12),
+                              child: const Icon(
+                                Icons.euro_rounded,
+                                color: Colors.white,
+                                size: 88,
+                              ),
+                            ),
                           )
                         ],
                       ),
